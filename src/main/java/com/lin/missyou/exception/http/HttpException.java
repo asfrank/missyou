840640555876b43f3d40eaba6bfe92a737f57805
lishuyu01 <lishuyu01@qiyi.com>
2020-11-08 22:10:37 +0800

    1. BannerController完成

---
+++ src/main/java/com/lin/missyou/exception/http/HttpException.java
@@ -0,0 +1,16 @@
+package com.lin.missyou.exception.http;
+
+public class HttpException extends RuntimeException {
+    protected Integer code;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public Integer getHttpStatusCode() {
+        return httpStatusCode;
+    }
+
+    protected Integer httpStatusCode = 500;
+}
+

b151a353f4907db968d185ff40d5308596b68f78
lishuyu01 <lishuyu01@qiyi.com>
2020-11-11 23:44:54 +0800

    1. 使用泛型做序列化和反序列化

--- src/main/java/com/lin/missyou/model/Sku.java
+++ src/main/java/com/lin/missyou/model/Sku.java
@@ -1,5 +1,7 @@
 package com.lin.missyou.model;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.lin.missyou.util.GenericAndJson;
 import com.lin.missyou.util.ListAndJson;
 import com.lin.missyou.util.MapAndJson;
 import lombok.Getter;
@@ -8,9 +10,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Getter
@@ -28,8 +28,25 @@ public class Sku extends BaseEntity {
 //    @Convert(converter = MapAndJson.class)
 //    private Map<String, Object> test;  // 测试单体json用
 
-    @Convert(converter = ListAndJson.class)
-    private List<Object> specs; // 规格 json字段
+//    @Convert(converter = ListAndJson.class)
+//    private List<Object> specs; // 规格 json字段
+
+    public List<Spec> getSpecs() {
+        if (this.specs == null) {
+            return Collections.emptyList();
+        }
+
+        return GenericAndJson.jsonToList(this.specs, new TypeReference<List<Spec>>() {});
+    }
+
+    public void setSpecs(List<Spec> specs) {
+        if (specs.isEmpty()) {
+            return;
+        }
+        this.specs = GenericAndJson.objectToJson(specs);
+    }
+
+    private String specs;
 
     private String code; // 前端使用，按规则拼接
     private Long stock;

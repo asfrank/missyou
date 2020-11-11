2917c92c9f28ccd3956d39e2c0b7dfd7da5034e5
lishuyu01 <lishuyu01@qiyi.com>
2020-11-11 16:24:14 +0800

    1. 单个json对象（test）映射为map

--- src/main/java/com/lin/missyou/model/Sku.java
+++ src/main/java/com/lin/missyou/model/Sku.java
@@ -1,14 +1,13 @@
 package com.lin.missyou.model;
 
+import com.lin.missyou.util.MapAndJson;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.util.Map;
 import java.util.Objects;
 
 @Entity
@@ -23,9 +22,14 @@ public class Sku extends BaseEntity {
     private String img;
     private String title;
     private Long spuId;
-    private String specs;
-    private String code;
+
+    @Convert(converter = MapAndJson.class)
+    private Map<String, Object> test;  // 测试单体json用
+
+    private String specs; // 规格 json字段
+
+    private String code; // 前端使用，按规则拼接
     private Long stock;
-    private Long categoryId;
+    private Long categoryId; // 冗余
     private Long rootCategoryId;
 }
---
+++ src/main/java/com/lin/missyou/util/MapAndJson.java
@@ -0,0 +1,38 @@
+package com.lin.missyou.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lin.missyou.exception.http.ServerErrorException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.util.HashMap;
+import java.util.Map;
+
+@Converter
+public class MapAndJson implements AttributeConverter<Map<String, Object>, String> {
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Override
+    public String convertToDatabaseColumn(Map<String, Object> map) {
+        try {
+            return mapper.writeValueAsString(map);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new ServerErrorException(9999);
+        }
+    }
+
+    @Override
+    public Map<String, Object> convertToEntityAttribute(String s) {
+        try {
+            return mapper.readValue(s, HashMap.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new ServerErrorException(9999);
+        }
+    }
+}
